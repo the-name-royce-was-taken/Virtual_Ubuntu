ed1a05b5da965a77cb96c49c4cab31baa5ffc172
the-name-royce-was-taken <royce2176@gmail.com>
2025-12-11 22:44:15 +1100

    Delete .devcontainer directory

--- .devcontainer/scripts/performance-tweaks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "vm.swappiness=10" >> /etc/sysctl.conf
-echo "vm.vfs_cache_pressure=50" >> /etc/sysctl.conf
-
-modprobe zswap || true
-echo 1 > /sys/module/zswap/parameters/enabled || true
-
-echo "performance" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor 2>/dev/null || true
