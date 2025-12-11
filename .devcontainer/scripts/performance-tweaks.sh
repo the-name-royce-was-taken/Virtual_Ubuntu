a6e3d716f46e560a62df806b04a5a52ea4beb5ee
the-name-royce-was-taken <royce2176@gmail.com>
2025-12-11 22:24:01 +1100

    Create performance-tweaks.sh

---
+++ .devcontainer/scripts/performance-tweaks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "vm.swappiness=10" >> /etc/sysctl.conf
+echo "vm.vfs_cache_pressure=50" >> /etc/sysctl.conf
+
+modprobe zswap || true
+echo 1 > /sys/module/zswap/parameters/enabled || true
+
+echo "performance" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor 2>/dev/null || true
